f29f38dd1eb4f117f7e4584641936c52f060d303
mymy0225 <xametal666@hotmail.co.jp>
2023-09-10 07:36:18 +0900

    init

---
+++ src/test/java/com/example/photoGallery/PhotoGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
